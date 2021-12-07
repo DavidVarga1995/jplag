23279c0c9e2999b8434124f482ed57f13280eb4b
DavidVarga1995 <varga95david@gmail.com>
2021-12-07 20:48:56 +0100

    fixes

--- jplag/src/main/java/jplag/options/util/ZipUtil.java
+++ jplag/src/main/java/jplag/options/util/ZipUtil.java
@@ -18,170 +18,159 @@ import java.util.zip.ZipOutputStream;
  */
 public class ZipUtil {
 
-  private static final Logger LOGGER = Logger.getLogger(ZipUtil.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(ZipUtil.class.getName());
+
+    private ZipUtil() {
+        throw new IllegalStateException("Utility class");
+    }
 
     /**
-     *
      * @param file file
-     * @param dest
-     *          Where the zipped file will be stored
+     * @param dest Where the zipped file will be stored
      * @return zipped file
      */
     public static File zip(File file, String dest) {
-    File zippedFile = FileUtils.getFile(dest + "/" + file.getName() + ".zip");
-    try {
-      FileOutputStream ops = new FileOutputStream(zippedFile);
-      ZipOutputStream zos = new ZipOutputStream(ops);
-      zip(file, zos, "");
-      zos.close();
-    } catch (IOException fnfex) {
-      // fnfex.printStackTrace();
-        LOGGER.log(Level.SEVERE, "Exception occur", fnfex);
-    }// ioex.printStackTrace();
-
+        File zippedFile = FileUtils.getFile(dest + "/" + file.getName() + ".zip");
+        try {
+            FileOutputStream ops = new FileOutputStream(zippedFile);
+            ZipOutputStream zos = new ZipOutputStream(ops);
+            zip(file, zos, "");
+            zos.close();
+        } catch (IOException fnfex) {
+            LOGGER.log(Level.SEVERE, "Exception occur in zip", fnfex);
+        }
         return zippedFile;
-  }
-
-  public static void zip(File file, ZipOutputStream zos, String prefix)
-  {
-    File[] entries = file.listFiles();
-    if (entries != null) {
-      for (File entry : entries) {
-          if (entry.isDirectory()) {
-              // generate directory entry
-              ZipEntry zi = new ZipEntry(prefix + entry.getName() + "/");
-              try {
-                  zos.putNextEntry(zi);
-                  zos.closeEntry();
-              } catch (IOException ioex) {
-                  // ioex.printStackTrace();
-                  LOGGER.log(Level.SEVERE, "Exception occur", ioex);
-              }
-              zip(entry, zos, prefix + entry.getName() + "/");
-          } else {
-              try {
-                  FileInputStream fis = new FileInputStream(entry);
-                  ZipEntry zi = new ZipEntry(prefix + entry.getName());
-                  zos.putNextEntry(zi);
-                  copystream(fis, zos);
-                  zos.closeEntry();
-              } catch (IOException ex) {
-                  // ex.printStackTrace();
-                  LOGGER.log(Level.SEVERE, "Exception occur", ex);
-              }// ioex.printStackTrace();
-
-          }
-      }
-  }
-  }
-
-  public static int copystream(InputStream in, OutputStream out)
-      throws IOException {
-
-    byte[] buffer = new byte[1024];
-    int len;
-    int totalsize = 0;
-
-    while ((len = in.read(buffer)) >= 0)
-    {
-        out.write(buffer, 0, len);
-        totalsize += len;
     }
-    totalsize += len;
 
-    in.close();
-    // out.close();
-    return totalsize;
-  }
+    public static void zip(File file, ZipOutputStream zos, String prefix) {
+        File[] entries = file.listFiles();
+        if (entries != null) {
+            for (File entry : entries) {
+                if (entry.isDirectory()) {
+                    // generate directory entry
+                    ZipEntry zi = new ZipEntry(prefix + entry.getName() + "/");
+                    try {
+                        zos.putNextEntry(zi);
+                        zos.closeEntry();
+                    } catch (IOException ioex) {
+                        LOGGER.log(Level.SEVERE, "Exception occur zip method", ioex);
+                    }
+                    zip(entry, zos, prefix + entry.getName() + "/");
+                } else {
+                    try (FileInputStream fis = new FileInputStream(entry)) {
+                        ZipEntry zi = new ZipEntry(prefix + entry.getName());
+                        zos.putNextEntry(zi);
+                        copystream(fis, zos);
+                        zos.closeEntry();
+                    } catch (IOException ex) {
+                        LOGGER.log(Level.SEVERE, "IO Exception occur", ex);
+                    }
 
-	/**
-	 * @param container
-	 *            all extracted file will be stored in container, which will be
-	 *            created in the destination
-	 * @param file file
-	 * @param destination
-	 *            where the file will be stored
-	 * @return total size of all unzipped files
-	 *
-	 */
-	public static int unzip(File file, String destination, String container) {
-		int totalsize = 0;
+                }
+            }
+        }
+    }
 
-        int THRESHOLD_ENTRIES = 10000;
-        int THRESHOLD_SIZE = 1000000000; // 1 GB
-        double THRESHOLD_RATIO = 10;
+    public static int copystream(InputStream in, OutputStream out)
+            throws IOException {
+
+        byte[] buffer = new byte[1024];
+        int len;
+        int totalsize = 0;
+
+        while ((len = in.read(buffer)) >= 0) {
+            out.write(buffer, 0, len);
+            totalsize += len;
+        }
+        totalsize += len;
+
+        in.close();
+        return totalsize;
+    }
+
+    /**
+     * @param container   all extracted file will be stored in container, which will be
+     *                    created in the destination
+     * @param file        file
+     * @param destination where the file will be stored
+     * @return total size of all unzipped files
+     */
+    public static int unzip(File file, String destination, String container) {
+        int totalsize = 0;
+
+        int thresholdEntries = 10000;
+        int thresholdSize = 1000000000; // 1 GB
+        double thresholdRatio = 10;
         int totalSizeArchive = 0;
         int totalEntryArchive = 0;
 
         File result = FileUtils.getFile(destination + File.separator + container);
-		if(result.mkdir())
-		destination = destination + File.separator + container + File.separator;
-		try {
-            ZipFile zipFile = new ZipFile(file);
-			Enumeration<? extends ZipEntry> entries = zipFile.entries();
-			while (entries.hasMoreElements()) {
-				ZipEntry ze = entries.nextElement();
-                File ze_f = FileUtils.getFile(destination, ze.getName());
-                String canonicalDestinationPath = ze_f.getCanonicalPath();
+        if (result.mkdir()) {
+            destination = destination + File.separator + container + File.separator;
+        }
+        try (ZipFile zipFile = new ZipFile(file)) {
+
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry ze = entries.nextElement();
+                File zeF = FileUtils.getFile(destination, ze.getName());
+                String canonicalDestinationPath = zeF.getCanonicalPath();
 
                 if (!canonicalDestinationPath.startsWith(destination)) {
                     throw new IOException("Entry is outside of the target directory");
                 }
-				if (ze.isDirectory())
-					if(!ze_f.mkdir())
+                if (ze.isDirectory())
+                    if (!zeF.mkdir()) {
                         break;
-				else {
-					// make sure directories exist in case the client
-					// didn't provide directory entries!
+                    } else {
+                        // make sure directories exist in case the client
+                        // didn't provide directory entries!
 
-                    InputStream in = null;
-                    BufferedOutputStream bos = null;
+                        InputStream in;
 
-					if((FileUtils.getFile(FileUtils.getFile(destination, ze.getName()).getParent())).mkdirs()){
-                        bos = new BufferedOutputStream(new FileOutputStream(FileUtils.getFile(
-                                destination, ze.getName())));
-                        in = zipFile.getInputStream(ze);
-                        totalsize += copystream(in, bos);
-                        bos.close();
-                    }
+                        if ((FileUtils.getFile(FileUtils.getFile(destination, ze.getName()).getParent())).mkdirs()) {
 
-                        totalEntryArchive ++;
+                            try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(FileUtils.getFile(
+                                    destination, ze.getName())))) {
+                                in = zipFile.getInputStream(ze);
+                                totalsize += copystream(in, bos);
 
-                        int nBytes;
-                        byte[] buffer = new byte[2048];
-                        int totalSizeEntry = 0;
+                                totalEntryArchive++;
 
-                        if(in != null) {
-                            while ((nBytes = in.read(buffer)) > 0) { // Compliant
-                                bos.write(buffer, 0, nBytes);
-                                totalSizeEntry += nBytes;
-                                totalSizeArchive += nBytes;
+                                int nBytes;
+                                byte[] buffer = new byte[2048];
+                                int totalSizeEntry = 0;
 
-                                double compressionRatio = (double) totalSizeEntry / ze.getCompressedSize();
-                                if (compressionRatio > THRESHOLD_RATIO) {
-                                    // ratio between compressed and uncompressed data is highly suspicious, looks like a Zip Bomb Attack
+                                while ((nBytes = in.read(buffer)) > 0) { // Compliant
+                                    bos.write(buffer, 0, nBytes);
+                                    totalSizeEntry += nBytes;
+                                    totalSizeArchive += nBytes;
+
+                                    double compressionRatio = (double) totalSizeEntry / ze.getCompressedSize();
+                                    if (compressionRatio > thresholdRatio) {
+                                        // ratio between compressed and uncompressed data is highly suspicious,
+                                        // looks like a Zip Bomb Attack
+                                        break;
+                                    }
+                                }
+
+                                if (totalSizeArchive > thresholdSize) {
+                                    // the uncompressed data size is too much for the application resource capacity
+                                    break;
+                                }
+
+                                if (totalEntryArchive > thresholdEntries) {
+                                    // too much entries in this archive, can lead to inodes exhaustion of the system
                                     break;
                                 }
                             }
                         }
 
-                        if(totalSizeArchive > THRESHOLD_SIZE) {
-                            // the uncompressed data size is too much for the application resource capacity
-                            break;
-                        }
-
-                        if(totalEntryArchive > THRESHOLD_ENTRIES) {
-                            // too much entries in this archive, can lead to inodes exhaustion of the system
-                            break;
-                        }
-
-				}
-			}
-			zipFile.close();
-		} catch (IOException ioex) {
-			// ioex.printStackTrace();
-            LOGGER.log(Level.SEVERE, "Exception occur", ioex);
-		}
-		return totalsize;
-	}
+                    }
+            }
+        } catch (IOException ioex) {
+            LOGGER.log(Level.SEVERE, "Exception occur in unzip", ioex);
+        }
+        return totalsize;
+    }
 }
